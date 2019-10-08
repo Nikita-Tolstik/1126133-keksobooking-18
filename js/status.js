90dd5575f1ac373e16f48a83021f1fed28d31767
Nikita <tolstik-nik@mail.ru>
2019-10-08 15:59:46 +0300

    Функция неактивного состояния страницы добавлена в глобальную область видимости

--- js/status.js
+++ js/status.js
@@ -4,30 +4,43 @@
   var PINHALF_WIDTH = 32;
   var ONE_GUEST = '1';
   var PINHALF_HEIGHT = 30;
+  var PIN_HEIGHT = 80;
 
-  // Неактивное состояние страницы
+
   var map = document.querySelector('.map');
   var adForm = document.querySelector('.ad-form');
   var adFormfieldsetAll = adForm.querySelectorAll('fieldset');
-
-  adFormfieldsetAll.forEach(function (element) {
-    element.disabled = true;
-  });
-
   var capacityOptionDisabled = document.querySelectorAll('#capacity option');
 
   var mapFilters = document.querySelector('.map__filters');
   var mapFiltersChildren = mapFilters.children;
   var mapFiltersAll = Array.from(mapFiltersChildren);
 
-  mapFiltersAll.forEach(function (element) {
-    element.disabled = true;
-  });
-
   var pinMainButton = map.querySelector('.map__pin--main');
   var inputAddress = document.querySelector('#address');
 
-  inputAddress.value = (pinMainButton.offsetLeft + PINHALF_WIDTH) + ', ' + (pinMainButton.offsetTop + PINHALF_HEIGHT);
+  var pinMainX = pinMainButton.style.left;
+  var pinMainY = pinMainButton.style.top;
+
+  // Функция неактивного состояния страницы
+  window.isInactive = function () {
+
+    adFormfieldsetAll.forEach(function (element) {
+      element.disabled = true;
+    });
+
+    mapFiltersAll.forEach(function (element) {
+      element.disabled = true;
+    });
+
+    pinMainButton.style.left = pinMainX;
+    pinMainButton.style.top = pinMainY;
+
+    inputAddress.value = (pinMainButton.offsetLeft + PINHALF_WIDTH) + ', ' + (pinMainButton.offsetTop + PINHALF_HEIGHT);
+  };
+
+
+  window.isInactive(); // Функция неактивного состояния страницы
 
   // Активное состояние страницы
   var openMap = function () {
@@ -59,27 +72,27 @@
       pinElement.classList.remove('hidden');
     });
 
-    inputAddress.value = (pinMainButton.offsetLeft + PINHALF_WIDTH) + ', ' + (pinMainButton.offsetTop);
+    inputAddress.value = (pinMainButton.offsetLeft + PINHALF_WIDTH) + ', ' + (pinMainButton.offsetTop + PIN_HEIGHT);
 
-    pinMainButton.removeEventListener('keydown', onEnterPress);
-    pinMainButton.removeEventListener('mousedown', onMainPinPress);
+    pinMainButton.removeEventListener('keydown', window.onEnterPress);
+    pinMainButton.removeEventListener('mousedown', window.onMainPinPress);
   };
 
 
-  var onEnterPress = function (evt) {
+  window.onEnterPress = function (evt) {
     if (evt.keyCode === window.util.ENTER_KEYCODE) {
       openMap();
     }
   };
 
-  var onMainPinPress = function (evtCl) {
+  window.onMainPinPress = function (evtCl) {
     evtCl.preventDefault();
     openMap();
   };
 
-  pinMainButton.addEventListener('mousedown', onMainPinPress);
+  pinMainButton.addEventListener('mousedown', window.onMainPinPress);
 
-  pinMainButton.addEventListener('keydown', onEnterPress);
+  pinMainButton.addEventListener('keydown', window.onEnterPress);
 
 
 })();
